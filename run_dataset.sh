cbc39338c776cf2c17cb8de76c53bba4fd1192d9
付嘉乐12 <jialefu23@gmail.com>
2025-05-31 22:16:08 +0800

    npu first commit

--- run_dataset.sh
+++ run_dataset.sh
@@ -1,8 +1,8 @@
 python ./main_dataset.py \
     dataset.name=humaneval \
     dataset.size=5 \
-    method=we \
+    method=cd_chef \
     method.model=Llama-2-7b-hf \
-    method.extra_model=vicuna-7b-v1.5 \
+    method.extra_model=llama-68m \
     method.gamma=5 \
-    method.lambda=0.5
+    method.alpha=0.1 \
--- run_pipeline.sh
+++ run_pipeline.sh
@@ -7,23 +7,38 @@ python -u ./pipeline.py \
     run_name=test \
 	visible_devices=[0] \
 	\
-    methods=[we_sd,we_chef] \
-    +dataset.name=[humaneval] \
-    +dataset.size=5 \
+    methods=[we,we_sd,we_chef,cd,cd_sd,cd_chef] \
+    +dataset.name=[humaneval,gsm8k,mmlu,cnndm] \
+    +dataset.size=50 \
     \
     +we.model=Llama-2-7b-hf \
     +we.extra_model=vicuna-7b-v1.5 \
-    +we.lambda=0.5 \
+    +we.lambda=[0.1,0.3,0.7,0.9] \
     +we.gamma=5 \
     \
     +we_sd.model=Llama-2-7b-hf \
     +we_sd.extra_model=vicuna-7b-v1.5 \
-    +we_sd.lambda=0.5 \
+    +we_sd.lambda=[0.1,0.3,0.7,0.9] \
     +we_sd.gamma=5 \
     \
     +we_chef.model=Llama-2-7b-hf \
     +we_chef.extra_model=vicuna-7b-v1.5 \
-    +we_chef.lambda=0.5 \
+    +we_chef.lambda=[0.1,0.3,0.7,0.9] \
     +we_chef.gamma=5 \
+    \
+    +cd.model=Llama-2-7b-hf \
+    +cd.extra_model=llama-68m \
+    +cd.alpha=[0.2,0.3,0.4,0.5] \
+    +cd.gamma=5 \
+    \
+    +cd_sd.model=Llama-2-7b-hf \
+    +cd_sd.extra_model=llama-68m  \
+    +cd_sd.alpha=[0.2,0.3,0.4,0.5] \
+    +cd_sd.gamma=5 \
+    \
+    +cd_chef.model=Llama-2-7b-hf \
+    +cd_chef.extra_model=llama-68m  \
+    +cd_chef.alpha=[0.2,0.3,0.4,0.5] \
+    +cd_chef.gamma=[1,5] \
     \
     hydra.run.dir=$run_dir
